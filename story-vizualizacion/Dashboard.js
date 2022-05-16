e4cdeaf5c07a91d054bdb5532d1330ab4f5c2c83
Andrea <riverandrea011@gmail.com>
2022-05-15 20:10:51 -0500

    subiendo archivos

---
+++ story-vizualizacion/Dashboard.js
@@ -0,0 +1,14 @@
+const graficocircular={
+    init() {
+        this.loadData()
+    }
+
+}
+
+loadData() {
+
+    var personas= XMLHttpRequest ()
+    xmlhttp.open('GET', 'story-visualizacion/proyecto-hepatitis.json')
+
+
+}
